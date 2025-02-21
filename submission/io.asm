ef4fe3c0ca40360d58dbdf74d18b45df4ec4e354
Tulio Panza <137923254+TulioPz@users.noreply.github.com>
2025-02-21 23:14:49 +0000

    Update io.asm

--- submission/io.asm
+++ submission/io.asm
@@ -9,24 +9,29 @@ MM /104 ; valor de y
 
 AD /102 ; soma x e y
 MM /200
-MM /10E
+MM /140
 ML SHIFT ; fica na forma 0X 00, sendo x o algarismo menos significativo
-MM /10A ; depuracacao
+MM /142 ; depuracacao
 SB VERIFICADOR ; verifica se a soma dos menos significativos eh >= A
-MM /10C ; depuracao
+MM /144 ; depuracao
 JN /040 ; caso negativo
 JP /050 ; caso positivo
 
 @ /0050
 LD /200
+MM /150
 SB SUBTRAICORRETOR
+MM /152
 AD SOMACORRETOR
+MM /154
 JP /042
 
 @ /0040
 LD /200   ; carrega soma
 AD TEMP   ; soma 3030
-PD /0001  ; tentativa de imprimir no monitor 
+MM /156
+PD /0001  ; tentativa de imprimir no terminal  
+MM /0060 
 HM /0000
 
 @ /0400
